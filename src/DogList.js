6a78e3d2bcc5dbcfb3f67b7ae5b596288267fad8
Shokhmurod <shokhpjwstk@gmail.com>
2022-12-06 06:47:43 +0100

    styled the app as much as I could. It could have been better of course!

--- src/DogList.js
+++ src/DogList.js
@@ -6,14 +6,16 @@ class DogList extends Component {
   render() {
     return (
       <div className="DogList">
-        <h1 className="display-1 text-center">DogList!</h1>
+        <h1 className="display-1 text-center" my-5>
+          DogList!
+        </h1>
 
         <div className="row">
           {this.props.dogs.map((d) => (
             <div className="Dog col-lg-4 text center" key={d.name}>
               <img src={d.src} alt={d.name} />
               <h3>
-                <Link to={`/dogs/${d.name}`} alt={d.name}>
+                <Link className="underline" to={`/dogs/${d.name}`} alt={d.name}>
                   {d.name}
                 </Link>
               </h3>
